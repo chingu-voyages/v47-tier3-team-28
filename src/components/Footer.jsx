fe85fd4e368b77349cfe8051a5f20832ce03c148
teodora-kocic <teodora.kocic1125@gmail.com>
2024-01-30 16:47:19 +0100

    feat(header-footer)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,46 @@
+import GithubImg from "../assets/github.svg";
+
 function Footer() {
-  return <div></div>;
+  return (
+    <footer className="bg-[#083556] text-white p-[15px] md:p-[24px] xl:mx-[80px] rounded-t-[22px] mx-[28px]">
+      <div className="flex md:flex-row flex-col items-center md:items-start w-full mb-[20px]">
+        <div className="md:w-1/2 p-[10px] text-center md:text-left">
+          <h2 className="font-bold mb-[10px] mt-[20px]">Mobile App</h2>
+          <p>
+            Learn or improve the most used language today by taking our courses,
+            examples, exercices and more
+          </p>
+        </div>
+
+        <div className="flex md:flex-row flex-col md:justify-evenly md:w-1/2 text-center md:text-left">
+          <div className="p-[10px]">
+            <h3 className="font-bold mb-[10px] mt-[20px]">Services</h3>
+            <div>
+              <p>Classes</p>
+              <p>Examples</p>
+              <p>Challenges</p>
+            </div>
+          </div>
+          <div className="p-[10px]">
+            <h3 className="font-bold mb-[10px] mt-[20px]">Team</h3>
+            <div className="text-center md:text-left">
+              <a
+                className="inline-block"
+                href="https://github.com/chingu-voyages/v47-tier3-team-28/tree/dev"
+                target="_blank"
+              >
+                <img className="w-[40px]" src={GithubImg} alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-center">
+        <p>© 2024 - V-47 Team 28 | All Rights Reserved</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,103 @@
 import { NavLink } from "react-router-dom";
-import "./Header.css";
+// import "./Header.css";
+import MenuIconDark from "../assets/menu-dark.png";
+import CloseMenuIconDark from "../assets/close-menu-dark.png";
+
+import { useEffect, useState } from "react";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenuIcon = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, []);
+
   return (
-    <header>
-      <div className="logo">
+    <header className=" p-[15px] md:p-[24px] flex flex-row justify-between items-center text-[#083556] bg-white rounded-t-[22px] my-[20px] mx-[28px] md:my-0 xl:mx-[80px] mx-[28px]">
+      <div className="logo flex items-center hidden md:block">
         <img src="" alt="" />
-        <h1>NAME</h1>
+        <h1 className="text-[28px] ml-8 md:ml-7 lg:text-[32px] font-bold">
+          NAME
+        </h1>
       </div>
 
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/courses">Courses</NavLink>
-          </li>
-          <li>
-            <NavLink to="/team">Team</NavLink>
-          </li>
-        </ul>
+      <nav className="flex flex-row justify-between	 items-center w-full md:w-auto">
+        <div className="w-10 h-10 mr-3.5 cursor-pointer md:hidden">
+          <div onClick={toggleMenuIcon}>
+            {!isOpen ? (
+              <img src={MenuIconDark} />
+            ) : (
+              <img src={CloseMenuIconDark} />
+            )}
+          </div>
+        </div>
 
-        <div>
-          <button>
-            <NavLink to="/signup">Get Started</NavLink>
-          </button>
+        <div className="flex flex-row justify-center items-center">
+          <ul
+            className={`flex flex-col md:flex-row items-center  absolute z-10 md:static right-0  left-0 transition-all duration-500 ease-in bg-[#20876E] md:bg-white mx-[28px] md:mx-0 md:mr-[20px] rounded-b-[22px] md:text-[#083556] text-white ${
+              isOpen ? "top-[110px] " : "top-[-490px]"
+            }`}
+          >
+            <li
+              onClick={toggleMenuIcon}
+              className="md:mr-8 text-xl md:my-0 py-3 mx-auto w-full text-center cursor-pointer md:py-0"
+            >
+              <NavLink
+                className="text-base lg:text-[24px] block md:inline"
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li
+              onClick={toggleMenuIcon}
+              className="md:mr-8 text-xl md:my-0 py-3 mx-auto w-full text-center cursor-pointer md:py-0"
+            >
+              <NavLink
+                className="text-base lg:text-[24px] block md:inline"
+                to="about"
+              >
+                About
+              </NavLink>
+            </li>
+            <li
+              onClick={toggleMenuIcon}
+              className="md:mr-8 text-xl md:my-0 py-3 mx-auto w-full text-center cursor-pointer md:py-0"
+            >
+              <NavLink
+                className="text-base lg:text-[24px] block md:inline"
+                to="/courses"
+              >
+                Courses
+              </NavLink>
+            </li>
+            <li
+              onClick={toggleMenuIcon}
+              className="md:mr-8 text-xl md:my-0 py-3 mx-auto w-full text-center cursor-pointer md:py-0"
+            >
+              <NavLink
+                className="text-base lg:text-[24px] block md:inline"
+                to="/team"
+              >
+                Team
+              </NavLink>
+            </li>
+          </ul>
+
+          <div className="md:mr-8 md:ml-8 text-xl md:my-0  bg-[#083556] text-white cursor-pointer rounded-[10px]">
+            <button>
+              <NavLink
+                className="text-base lg:text-[24px] md:my-0 md:py-[10px] md:px-[16px] py-[6px] px-[10px] inline-block"
+                to="/signup"
+              >
+                Get Started
+              </NavLink>
+            </button>
+          </div>
         </div>
       </nav>
     </header>
