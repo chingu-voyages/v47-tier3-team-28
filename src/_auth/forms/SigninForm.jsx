c1ea1eb52efc4d0b0dde13e65de9b79f91276708
teodora-kocic <teodora.kocic1125@gmail.com>
2024-02-11 21:54:55 +0100

    feat(auth): sign up and log in

--- src/_auth/forms/SigninForm.jsx
+++ src/_auth/forms/SigninForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Paper, Typography, TextField, Button } from "@mui/material";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
@@ -8,7 +8,38 @@ import FormControl from "@mui/material/FormControl";
 import Checkbox from "@mui/material/Checkbox";
 import Hidden from "@mui/material/Hidden";
 
-const Signup = () => {
+import { useNavigate } from "react-router-dom";
+import { logIn } from "../../api/auth";
+
+import { useSelector, useDispatch } from "react-redux";
+import { setUser } from "../../state/user";
+
+const Signin = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: undefined,
+    password: undefined,
+  });
+
+  const addToFormData = (field, value) => {
+    setFormData({
+      ...formData,
+      [field]: value,
+    });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    logIn(formData).then((user) => {
+      dispatch(setUser(user));
+      localStorage.setItem("userToken", user.token);
+      navigate("/");
+    });
+  };
+
   const paperStyle = {
     padding: "30px 20px",
     width: "100%",
@@ -29,44 +60,82 @@ const Signup = () => {
   };
 
   return (
-    <Grid container style={{ backgroundColor: "#20876E" }}>
-      <Grid item lg={6} md={12} xs={12}>
-        <Paper
-          elevation={0}
-          style={{
-            ...paperStyle,
-            background: "transparent",
-            boxShadow: "none",
-          }}
-        >
-          <Grid align="center">
-            <h2 style={headerStyle}>SIGN IN</h2>
-            <Typography
-              variant="caption"
-              gutterBottom
-              style={{ fontSize: "16px" }}
-            >
-              Sign in to account
-            </Typography>
-          </Grid>
-          <form>
-            <TextField
-              fullWidth
-              placeholder="Enter your username"
-              margin="normal"
-              InputProps={{ style: inputPropsStyle }}
-              InputLabelProps={{ ...inputLabelPropsStyle }}
-            />
+    <div className="xl:mx-[80px] rounded-[22px] mx-[28px] overflow-hidden	mb-[20px]">
+      <Grid container style={{ backgroundColor: "#20876E" }}>
+        <Grid item lg={6} md={12} xs={12}>
+          <Paper
+            elevation={0}
+            style={{
+              ...paperStyle,
+              background: "transparent",
+              boxShadow: "none",
+            }}
+          >
+            <Grid align="center">
+              <h2 style={headerStyle}>SIGN IN</h2>
+              <Typography
+                variant="caption"
+                gutterBottom
+                style={{ fontSize: "16px" }}
+              >
+                Sign in to account
+              </Typography>
+            </Grid>
+            <form onSubmit={submitForm}>
+              <TextField
+                type="email"
+                fullWidth
+                placeholder="Enter your email"
+                margin="normal"
+                InputProps={{ style: inputPropsStyle }}
+                InputLabelProps={{ ...inputLabelPropsStyle }}
+                onChange={(e) => addToFormData("email", e.target.value)}
+              />
 
-            <TextField
-              fullWidth
-              margin="normal"
-              placeholder="Enter your password"
-              InputProps={{ style: inputPropsStyle }}
-              InputLabelProps={{ ...inputLabelPropsStyle }}
-            />
+              <TextField
+                type="password"
+                fullWidth
+                margin="normal"
+                placeholder="Enter your password"
+                InputProps={{ style: inputPropsStyle }}
+                InputLabelProps={{ ...inputLabelPropsStyle }}
+                onChange={(e) => addToFormData("password", e.target.value)}
+              />
 
-            <div>
+              <div>
+                <Grid
+                  container
+                  spacing={2}
+                  justifyContent={{ xs: "center", md: "flex-start" }}
+                >
+                  <Grid item xs={8} lg={6} md={6}>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      style={{
+                        width: "100%", // For mobile
+                        backgroundColor: "white",
+                        color: "#20876E",
+                        marginTop: "20px",
+                      }}
+                      margin="normal"
+                    >
+                      SIGN IN
+                    </Button>
+                  </Grid>
+                </Grid>
+              </div>
+              <Grid
+                container
+                spacing={2}
+                justifyContent={{ xs: "center", md: "flex-start" }}
+                marginTop="normal"
+              >
+                <Grid item xs={8} lg={8} md={6}>
+                  <p style={{ marginTop: "20px" }}>Don't have an account?</p>
+                </Grid>
+              </Grid>
+
               <Grid
                 container
                 spacing={2}
@@ -74,78 +143,45 @@ const Signup = () => {
               >
                 <Grid item xs={8} lg={6} md={6}>
                   <Button
-                    type="submit"
+                    onClick={() => navigate("/signup")}
+                    type="button"
                     variant="contained"
                     style={{
                       width: "100%", // For mobile
                       backgroundColor: "white",
                       color: "#20876E",
-                      marginTop: "20px",
+                      marginTop: "10px",
                     }}
                     margin="normal"
                   >
-                    SIGN IN
+                    Register New Account
                   </Button>
                 </Grid>
               </Grid>
-            </div>
-            <Grid
-              container
-              spacing={2}
-              justifyContent={{ xs: "center", md: "flex-start" }}
-              marginTop="normal"
-            >
-              <Grid item xs={8} lg={8} md={6}>
-                <p style={{ marginTop: "20px" }}>
-                  I forgot my password, click here to reset
-                </p>
-              </Grid>
-            </Grid>
-
-            <Grid
-              container
-              spacing={2}
-              justifyContent={{ xs: "center", md: "flex-start" }}
-            >
-              <Grid item xs={8} lg={6} md={6}>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  style={{
-                    width: "100%", // For mobile
-                    backgroundColor: "white",
-                    color: "#20876E",
-                    marginTop: "20px",
-                  }}
-                  margin="normal"
-                >
-                  Register New Account
-                </Button>
-              </Grid>
-            </Grid>
-          </form>
-        </Paper>
-      </Grid>
-
-      <Hidden smDown xsDown mdDown>
-        <Grid
-          item
-          lg={6}
-          md={12}
-          xs={12}
-          container
-          alignItems="center"
-          justifyContent="center"
-        >
-          <img
-            src={"/public/authimage1.svg"}
-            alt="Your Image"
-            style={{ width: "80%", height: "auto" }}
-          />
+            </form>
+          </Paper>
         </Grid>
-      </Hidden>
-    </Grid>
+
+        <Hidden smDown xsDown mdDown>
+          <Grid
+            item
+            lg={6}
+            md={12}
+            xs={12}
+            container
+            alignItems="center"
+            justifyContent="center"
+          >
+            <img
+              src={"/public/authimage1.svg"}
+              alt="Your Image"
+              style={{ width: "80%", height: "auto" }}
+            />
+          </Grid>
+        </Hidden>
+      </Grid>
+    </div>
   );
 };
 
-export default Signup;
+export default Signin;
--- src/_auth/forms/SignupForm.jsx
+++ src/_auth/forms/SignupForm.jsx
@@ -1,137 +1,305 @@
-import React from "react";
-import {
-  Grid,
-  Paper,
-  Typography,
-  TextField,
-  Button,
-} from "@mui/material";
+import React, { useState } from "react";
+import { Grid, Paper, Typography, TextField, Button } from "@mui/material";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 
 import Checkbox from "@mui/material/Checkbox";
-import Hidden from '@mui/material/Hidden'
+import Hidden from "@mui/material/Hidden";
+
+import { useNavigate } from "react-router-dom";
+
+import { signUpStudent, signUpInstructor } from "../../api/auth";
+
+import { useSelector, useDispatch } from "react-redux";
+import { setUser } from "../../state/user";
 
 const Signup = () => {
-  const paperStyle = { padding: "30px 20px", width: "100%", maxWidth: 600, margin: "20px auto", color: "white", };
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    firstName: undefined,
+    profilePhoto: undefined,
+    lastName: undefined,
+    dob: undefined,
+    email: undefined,
+    password: undefined,
+    major: undefined,
+    role: "student",
+    bio: undefined,
+    years_of_experience: undefined,
+    specialization: undefined,
+  });
+
+  const addToFormData = (field, value) => {
+    setFormData({
+      ...formData,
+      [field]: value,
+    });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    if (formData.role === "student") {
+      signUpStudent(formData).then((student) => {
+        dispatch(setUser(student));
+        localStorage.setItem("userToken", student.token);
+        navigate("/");
+      });
+    } else {
+      signUpInstructor(formData).then((instructor) => {
+        dispatch(setUser(instructor));
+        localStorage.setItem("userToken", instructor.token);
+        navigate("/");
+      });
+    }
+  };
+
+  const paperStyle = {
+    padding: "30px 20px",
+    width: "100%",
+    maxWidth: 600,
+    margin: "20px auto",
+    color: "white",
+  };
   const headerStyle = { margin: 10 };
   const marginTop = { marginTop: 15 };
   const inputPropsStyle = {
     height: 40,
-    backgroundColor: 'white', 
+    backgroundColor: "white",
   };
-  const radioStyle = { color: 'white' }; 
-  const checkboxStyle = { color: 'white' }; 
+  const textareaPropsStyle = {
+    backgroundColor: "white",
+  };
+  const radioStyle = { color: "white" };
+  const checkboxStyle = { color: "white" };
 
-  const inputLabelPropsStyle = { shrink: false, style: { color: 'black', transform: 'none' } };
+  const inputLabelPropsStyle = {
+    shrink: false,
+    style: { color: "black", transform: "none" },
+  };
 
   return (
-    <Grid container style={{ backgroundColor: "#20876E" }}>
-    
-      <Grid item lg={6} md={12} xs={12}>
-      <Paper elevation={0} style={{ ...paperStyle, background: "transparent", boxShadow: "none" }}>
-          <Grid align="center">
-            
-            <h2 style={headerStyle}>SIGN UP</h2>
-            <Typography variant="caption" gutterBottom style={{ fontSize: "16px" }}>
-              Please fill this form to create an account!
-            </Typography>
-          </Grid>
-          <form>
-            <TextField fullWidth placeholder="Enter your name" margin="normal" InputProps={{ style: inputPropsStyle }} 
-            InputLabelProps={{ ...inputLabelPropsStyle }}
-            
-            />
-            <TextField fullWidth placeholder="Enter your email" margin="normal" InputProps={{ style: inputPropsStyle }} 
-            InputLabelProps={{ ...inputLabelPropsStyle }}/>
-
-<TextField
-              fullWidth
-              margin="normal"
-              placeholder="Enter your username"
-              InputProps={{ style: inputPropsStyle }} 
-              InputLabelProps={{ ...inputLabelPropsStyle }}
-            />
-            <TextField
-              fullWidth
-              margin="normal"
-              placeholder="Enter your password"
-              InputProps={{ style: inputPropsStyle }} 
-              InputLabelProps={{ ...inputLabelPropsStyle }}
-            />
-
-            <FormControl fullWidth component="fieldset" style={marginTop}>
-              <RadioGroup
-              
-                style={{ display: "initial" }}
+    <div className="xl:mx-[80px] rounded-[22px] mx-[28px] overflow-hidden mb-[20px]">
+      <Grid container style={{ backgroundColor: "#20876E" }}>
+        <Grid item lg={6} md={12} xs={12}>
+          <Paper
+            elevation={0}
+            style={{
+              ...paperStyle,
+              background: "transparent",
+              boxShadow: "none",
+            }}
+          >
+            <Grid align="center">
+              <h2 style={headerStyle}>SIGN UP</h2>
+              <Typography
+                variant="caption"
+                gutterBottom
+                style={{ fontSize: "16px" }}
               >
-                <FormControlLabel
-                  value="user"
-                  control={<Radio style={radioStyle} />}
-                  label="As user"
-                />
-                <FormControlLabel
-                  value="admin"
-                  control={<Radio style={radioStyle} />}
-                  label="As admin"
-                />
-              </RadioGroup>
-            </FormControl>
-
-            <FormControlLabel
-             
-              control={<Checkbox style={checkboxStyle} name="checkedA" />}
-              label="I accept the terms and conditions."
-            />
-            <div>
-            <Grid container spacing={2}  justifyContent={{ xs: 'center', md: 'flex-start' }}>
-                <Grid item xs={8} lg={6} md={6}>
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    style={{
-                      width: "100%", // For mobile
-                      backgroundColor: "white",
-                      color: "#20876E",
-                      marginTop: "20px",
-                    }}
-                    margin="normal"
-                  >
-                    Sign up
-                  </Button>
-                </Grid>
+                Please fill this form to create an account!
+              </Typography>
             </Grid>
-            </div>
-            <Grid container spacing={2} justifyContent={{ xs: 'center', md: 'flex-start' }}>
+            <form onSubmit={submitForm}>
+              <TextField
+                type="text"
+                fullWidth
+                placeholder="Enter your first name"
+                margin="normal"
+                InputProps={{ style: inputPropsStyle }}
+                InputLabelProps={{ ...inputLabelPropsStyle }}
+                onChange={(e) => addToFormData("firstName", e.target.value)}
+              />
+              <TextField
+                type="text"
+                fullWidth
+                placeholder="Enter your last name"
+                margin="normal"
+                InputProps={{ style: inputPropsStyle }}
+                InputLabelProps={{ ...inputLabelPropsStyle }}
+                onChange={(e) => addToFormData("lastName", e.target.value)}
+              />
+
+              <TextField
+                type="email"
+                fullWidth
+                margin="normal"
+                placeholder="Enter your email"
+                InputProps={{ style: inputPropsStyle }}
+                InputLabelProps={{ ...inputLabelPropsStyle }}
+                onChange={(e) => addToFormData("email", e.target.value)}
+              />
+              <TextField
+                type="password"
+                fullWidth
+                margin="normal"
+                placeholder="Enter your password"
+                InputProps={{ style: inputPropsStyle }}
+                InputLabelProps={{ ...inputLabelPropsStyle }}
+                onChange={(e) => addToFormData("password", e.target.value)}
+              />
+
+              <TextField
+                type="date"
+                fullWidth
+                placeholder="Enter your date of birth"
+                margin="normal"
+                InputProps={{ style: inputPropsStyle }}
+                InputLabelProps={{ ...inputLabelPropsStyle }}
+                onChange={(e) => addToFormData("dob", e.target.value)}
+              />
+
+              {formData.role == "instructor" ? (
+                <div>
+                  <TextField
+                    type="textarea"
+                    multiline
+                    rows={4}
+                    fullWidth
+                    placeholder="Enter your biography"
+                    margin="normal"
+                    InputProps={{ style: textareaPropsStyle }}
+                    InputLabelProps={{ ...inputLabelPropsStyle }}
+                    onChange={(e) => addToFormData("bio", e.target.value)}
+                  />
+
+                  <TextField
+                    type="text"
+                    fullWidth
+                    placeholder="Specialization"
+                    margin="normal"
+                    InputProps={{ style: inputPropsStyle }}
+                    InputLabelProps={{ ...inputLabelPropsStyle }}
+                    onChange={(e) =>
+                      addToFormData("specialization", e.target.value)
+                    }
+                  />
+
+                  <TextField
+                    type="number"
+                    fullWidth
+                    placeholder="Years of experience"
+                    margin="normal"
+                    InputProps={{ style: inputPropsStyle }}
+                    InputLabelProps={{ ...inputLabelPropsStyle }}
+                    onChange={(e) =>
+                      addToFormData("years_of_experience", e.target.value)
+                    }
+                  />
+                </div>
+              ) : (
+                ""
+              )}
+
+              <FormControl fullWidth component="fieldset" style={marginTop}>
+                <RadioGroup
+                  style={{ display: "initial" }}
+                  value={formData.role}
+                  defaultValue="student"
+                  onChange={(e) => addToFormData("role", e.target.value)}
+                >
+                  <FormControlLabel
+                    value="student"
+                    control={<Radio style={radioStyle} />}
+                    label="As student"
+                  />
+                  <FormControlLabel
+                    value="instructor"
+                    control={<Radio style={radioStyle} />}
+                    label="As instructor"
+                  />
+                </RadioGroup>
+              </FormControl>
+
+              <FormControlLabel
+                control={<Checkbox style={checkboxStyle} name="checkedA" />}
+                label="I accept the terms and conditions."
+              />
+              <div>
+                <Grid
+                  container
+                  spacing={2}
+                  justifyContent={{ xs: "center", md: "flex-start" }}
+                >
+                  <Grid item xs={8} lg={6} md={6}>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      style={{
+                        width: "100%", // For mobile
+                        backgroundColor: "white",
+                        color: "#20876E",
+                        marginTop: "20px",
+                      }}
+                      margin="normal"
+                    >
+                      Sign up
+                    </Button>
+                  </Grid>
+                </Grid>
+              </div>
+              <Grid
+                container
+                spacing={2}
+                justifyContent={{ xs: "center", md: "flex-start" }}
+              >
+                {/* <Grid item xs={8} lg={8} md={6}>
+                  <p style={{ marginTop: "20px", textAlign: "center" }}>
+                    Already have an account?
+                  </p>
+                </Grid> */}
                 <Grid item xs={8} lg={6} md={6}>
+                  <p
+                    style={{
+                      marginTop: "20px",
+                      textAlign: "center",
+                      marginBottom: "10px",
+                    }}
+                  >
+                    Already have an account?
+                  </p>
                   <Button
-                    type="submit"
+                    onClick={() => navigate("/login")}
+                    type="button"
                     variant="contained"
                     style={{
                       width: "100%", // For mobile
                       backgroundColor: "white",
                       color: "#20876E",
-                      marginTop: "20px",
                     }}
                     margin="normal"
                   >
                     Log in to your account
                   </Button>
                 </Grid>
-                </Grid>
-          </form>
-        </Paper>
-      </Grid>
-
-      
-      <Hidden smDown xsDown mdDown>
-        <Grid item lg={6} md={12} xs={12} container alignItems="center" justifyContent="center">
-          <img src={'/public/authimage1.svg'} alt="Your Image" style={{ width: "80%", height: "auto" }} />
+              </Grid>
+            </form>
+          </Paper>
         </Grid>
-      </Hidden>
-    </Grid>
+
+        <Hidden smDown xsDown mdDown>
+          <Grid
+            item
+            lg={6}
+            md={12}
+            xs={12}
+            container
+            alignItems="center"
+            justifyContent="center"
+          >
+            <img
+              src={"/public/authimage1.svg"}
+              alt="Your Image"
+              style={{ width: "80%", height: "auto" }}
+            />
+          </Grid>
+        </Hidden>
+      </Grid>
+    </div>
   );
 };
 
